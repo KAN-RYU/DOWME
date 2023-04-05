1668ca12fc7386003153a5b4104b8e9d92507c10
PureBlueWater <chsrlyhsn@gmail.com>
2023-04-05 16:52:38 +0900

    FIX: bean

--- src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
+++ src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@ComponentScan(basePackages = {"com.puresushi.cse364project.data"})
+@EnableMongoRepositories(basePackages = {"com.puresushi.cse364project.data"})
 @SpringBootApplication()
 public class RESTExerciseApplication {
     public static void main(String... args) {
