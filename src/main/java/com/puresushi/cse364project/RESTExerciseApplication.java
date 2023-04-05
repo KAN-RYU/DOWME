089c2402ee0f67ecb8e4ec8c5f0e5ca16c88c603
PureBlueWater <chsrlyhsn@gmail.com>
2023-04-05 16:18:08 +0900

    FIX: ComponentScan

--- src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
+++ src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
@@ -3,7 +3,9 @@ package com.puresushi.cse364project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.puresushi.cse364project.data.EmployeeDB"})
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class RESTExerciseApplication {
     public static void main(String... args) {
