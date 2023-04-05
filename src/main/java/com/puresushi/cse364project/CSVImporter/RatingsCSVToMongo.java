41f495f08e1118795f1399cd1c6ae85115484939
PureBlueWater <chsrlyhsn@gmail.com>
2023-04-06 02:53:07 +0900

    ADD: indicating DB initialization progress

--- src/main/java/com/puresushi/cse364project/CSVImporter/RatingsCSVToMongo.java
+++ src/main/java/com/puresushi/cse364project/CSVImporter/RatingsCSVToMongo.java
@@ -1,16 +1,21 @@
 package com.puresushi.cse364project.CSVImporter;
 
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvValidationException;
 import com.puresushi.cse364project.data.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 public class RatingsCSVToMongo {
 
+    private  final Logger log = LoggerFactory.getLogger(getClass());
     private final MovieRepository movieRepository;
     private final RatingRepository ratingRepository;
 
@@ -20,14 +25,15 @@ public class RatingsCSVToMongo {
     }
 
     public void readRatingCSV() {
-        String[] ratingInfo;
-
         try {
             CSVReader csvReader = new CSVReader(new InputStreamReader(new FileInputStream("/root/project/DOWME/data/ratings.csv")));
+            List<String[]> ratings = csvReader.readAll();
+            int index = 0;
+            int length = ratings.size();
 
-            do {
+            for (String[] ratingInfo : ratings) {
                 ratingInfo = csvReader.readNext();
-
+                if (index % 5000 == 0) log.info(index + " / " + length + " loading");
                 if (ratingInfo != null) {
                     int userId = Integer.parseInt(ratingInfo[0]);
                     int movieId = Integer.parseInt(ratingInfo[1]);
@@ -40,12 +46,14 @@ public class RatingsCSVToMongo {
                     m.setTotalRating(m.getTotalRating() + ratingInt);
                     movieRepository.save(m);
                 }
-            }while (ratingInfo != null);
+            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (CsvValidationException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CsvException e) {
             throw new RuntimeException(e);
         }
     }
