66310e528bae2743084d19b4cf6ad4ef6535df85
PureBlueWater <chsrlyhsn@gmail.com>
2023-04-05 16:21:39 +0900

    FIX: SpringBootApplication annotation

--- src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
+++ src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.puresushi.cse364project.data.EmployeeDB"})
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication()
 public class RESTExerciseApplication {
     public static void main(String... args) {
         SpringApplication.run(RESTExerciseApplication.class, args);
