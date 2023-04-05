82c89227460335f1109bbc576611358552bb5a89
PureBlueWater <chsrlyhsn@gmail.com>
2023-04-05 23:38:33 +0900

    FIX: Job error

--- src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
+++ src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages = {"com.puresushi.cse364project.data"})
-@SpringBootApplication()
+@EnableMongoRepositories(basePackages = {"com.puresushi.cse364project.data", "com.puresushi.cse364project.CSVImporter"})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class RESTExerciseApplication {
     public static void main(String... args) {
         SpringApplication.run(RESTExerciseApplication.class, args);
