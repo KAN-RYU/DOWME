3a686990584a78e21427017abcc8ff030d17ce05
PureBlueWater <chsrlyhsn@gmail.com>
2023-05-15 15:54:21 +0900

    ADD: Get Bus Id method

--- src/main/java/com/puresushi/cse364project/data/BusTimeController.java
+++ src/main/java/com/puresushi/cse364project/data/BusTimeController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class BusTimeController {
@@ -16,6 +19,24 @@ public class BusTimeController {
 
     public BusTimeController(BusTimeRepository busTimeRepository) {this.busTimeRepository = busTimeRepository;}
 
+    // Get all Bus ids
+    @GetMapping("/bus")
+    public List<String> getBusIds() {
+        List<String> result = new ArrayList<>();
+        List<BusTime> busTimeList = busTimeRepository.findAll();
+
+        for (BusTime busTime: busTimeList) {
+            result.add(busTime.getBusId());
+        }
+
+        Set<String> set = new HashSet<>(result);
+        result.clear();
+        result.addAll(set);
+
+        return result;
+    }
+
+    // Get Timetable of input bus ID
     @GetMapping("/bus/{busId}")
     public BusData getBusData(@PathVariable String busId) {
         List<BusTime> busTimeList = busTimeRepository.findByBusId(busId);
@@ -25,5 +46,7 @@ public class BusTimeController {
         }
         return result;
     }
+
+
 
 }
