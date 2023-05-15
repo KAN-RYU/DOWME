3d8590e7e908a9e436b9fc4d8f0974781d230b36
PureBlueWater <chsrlyhsn@gmail.com>
2023-05-15 19:27:20 +0900

    ADD: GET meal menu

--- src/main/java/com/puresushi/cse364project/data/MealMenuController.java
+++ src/main/java/com/puresushi/cse364project/data/MealMenuController.java
@@ -1,4 +1,38 @@
 package com.puresushi.cse364project.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MealMenuController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final MealMenuRepository mealMenuRepository;
+
+    public MealMenuController(MealMenuRepository mealMenuRepository) { this.mealMenuRepository = mealMenuRepository;}
+
+    // Get meal with date
+    @GetMapping("/meal/{date}")
+    public List<MealMenu> getMealWithDate(@PathVariable int date) {
+        return mealMenuRepository.findByDate(date);
+    }
+
+    // Get meal with date, time
+    @GetMapping("/meal/{date}/{time}")
+    public List<MealMenu> getMealWithDateAndTime(@PathVariable int date, @PathVariable String time) {
+        return mealMenuRepository.findByDateAndTime(date, time);
+    }
+
+    // Get meal with date, time, restaurant
+    @GetMapping("/meal/{date}/{time}/{restaurant}")
+    public List<MealMenu> getMealWithDateAndTimeAndRestaurant(@PathVariable int date, @PathVariable String time, @PathVariable String restaurant) {
+        return mealMenuRepository.findByDateAndTimeAndRestaurant(date, time, restaurant);
+    }
+
 }
