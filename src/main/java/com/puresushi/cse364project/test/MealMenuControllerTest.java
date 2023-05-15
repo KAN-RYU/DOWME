0ff94dc118a90770a9e38847ee8227594324442f
PureBlueWater <chsrlyhsn@gmail.com>
2023-05-15 21:19:42 +0900

    FIX: test

--- src/main/java/com/puresushi/cse364project/test/MealMenuControllerTest.java
+++ src/main/java/com/puresushi/cse364project/test/MealMenuControllerTest.java
@@ -3,13 +3,15 @@ package com.puresushi.cse364project.test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class MealMenuControllerTest {
 
     @Autowired
