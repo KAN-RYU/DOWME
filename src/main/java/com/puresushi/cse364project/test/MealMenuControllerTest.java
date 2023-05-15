344d8f4e568da706ce90ed4a2708b3a3ad0522a3
PureBlueWater <chsrlyhsn@gmail.com>
2023-05-15 21:33:13 +0900

    ADD: import test

--- src/main/java/com/puresushi/cse364project/test/MealMenuControllerTest.java
+++ src/main/java/com/puresushi/cse364project/test/MealMenuControllerTest.java
@@ -1,14 +1,23 @@
 package com.puresushi.cse364project.test;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.puresushi.cse364project.data.MealMenu;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,7 +28,16 @@ public class MealMenuControllerTest {
 
     @Test
     public void testNewMealMenu() throws Exception {
-        return;
+        String url = "/meal";
+        MealMenu menu = new MealMenu(230522, "Lunch", "Korean", "Dormitory",
+                "rice / kimchi");
+        String json = new ObjectMapper().writeValueAsString(menu);
+        ResultActions actions = mvc.perform(put(url).contentType(MediaType.APPLICATION_JSON)
+                .content(json));
+
+        actions.andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$", equalTo("230522")))
+                .andDo(print());
     }
 
 }
