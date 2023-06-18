b080934f06c16e71fbd356fe86a559c55e16f552
PureBlueWater <chsrlyhsn@gmail.com>
2023-06-18 13:57:28 +0900

    for tomcat

--- src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
+++ src/main/java/com/puresushi/cse364project/RESTExerciseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableMongoRepositories(basePackages = {"com.puresushi.cse364project.data", "com.puresushi.cse364project.CSVImporter"})
 @EnableScheduling
 @SpringBootApplication()
-public class RESTExerciseApplication {
+public class RESTExerciseApplication extends SpringBootServletInitializer {
     public static void main(String... args) {
         SpringApplication.run(RESTExerciseApplication.class, args);
     }
